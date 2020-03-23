193bcccdc225d4c8a8f0d90ab937c1d58145a388
LeTadas <legotatadas@gmail.com>
2020-03-23 11:38:56 +0100

    Some changes

--- udp_server.js
+++ udp_server.js
@@ -6,6 +6,7 @@ var ipAddress = "192.168.178.94"
 var port = 2222
 
 server.bind(port, ipAddress);
+// server.connect(port, ipAddress);
 
 // Emits errors
 server.on('error', function(error) {
@@ -13,32 +14,47 @@ server.on('error', function(error) {
     server.close();
 });
 
+// Message
+var data = Buffer.from('New Android message');
+
 // Emits message
 server.on('message', function(msg, info) {
     console.log('Data received from server: ' + msg.toString());
     console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port);
 
-    // // Sending message
-    // server.send(msg, info.port, 'localhost', function(error) {
-    //     if(error) {
-    //         client.close();
-    //     }else {
-    //         console.log('Data sent !!');
-    //     }
-    // });
+    if (msg.toString() === 'Android') {
+      
+    }
+
+    // Sending message
+    server.send(data, info.port, info.address, function(error) {
+        if(error) {
+            client.close();
+        }else {
+            console.log('Data sent !!');  
+        }
+    });
 });
 
-// Message
-var data = Buffer.from('Android message');
+//Sending message
 
-// Sending message
-server.send(data, port, ipAddress, function(error) {
+// server.send(data, port, ipAddress, function(error) {
+//         if(error) {
+//             client.close();
+//         }else {
+//             console.log('Data sent !!');
+//         }
+// });
+
+setTimeout(function(){
+  server.send(data, port, ipAddress, function(error) {
     if(error) {
         client.close();
     }else {
         console.log('Data sent !!');
     }
 });
+},8000);
 
 // Emits when socket is ready and listening for datagram msgs
 server.on('listening',function(){
@@ -46,9 +62,9 @@ server.on('listening',function(){
     var port = address.port;
     var family = address.family;
     var ipaddr = address.address;
-    console.log('Server is listening at port' + port);
-    console.log('Server ip :' + ipaddr);
-    console.log('Server is IP4/IP6 : ' + family);
+    console.log('Server is listening at port: ' + port);
+    console.log('Server ip: ' + ipaddr);
+    console.log('Server is IP4/IP6: ' + family);
   });
   
   //emits after the socket is closed using socket.close();
@@ -58,7 +74,7 @@ server.on('listening',function(){
   
   setTimeout(function(){
   server.close();
-  },8000);
+  },998000);
   
   // -------------------- udp client ----------------
   
